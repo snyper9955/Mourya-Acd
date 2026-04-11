99a04bb84a48b1a4fe65cd44227af25fc6795ec5
snyper9955 <162612396+snyper9955@users.noreply.github.com>
2026-04-11 23:07:14 +0530

    hi

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: '/',
     plugins: [react(), tailwindcss()],
     server: {
       proxy: {
